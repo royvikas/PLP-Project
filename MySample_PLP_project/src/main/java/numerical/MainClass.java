3c09e5eb83e54fc6ea8a21bae5102460b754c937
aishukb96 <40978931+aishukb96@users.noreply.github.com>
2018-08-06 15:35:17 +0530

    number to string converted by KB

---
+++ MySample_PLP_project/src/main/java/numerical/MainClass.java
@@ -0,0 +1,18 @@
+package numerical;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		NumberToWordsConverter num=new NumberToWordsConverter();
+		Scanner s=new Scanner(System.in);
+	    System.out.println("Enter a number to convert into word format");
+		n =s.nextInt();
+		System.out.println(NumberFormat.getInstance().format(n) + "='" + num.convert(n) + "'");
+		s.close();
+	}
+
+}
