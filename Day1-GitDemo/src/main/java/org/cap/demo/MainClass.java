b4fc839fef76484195d6d00591e3a85aacaf6ad6
vr32 <vr32@DIN73002987.corp.capgemini.com>
2018-08-06 10:33:36 +0530

    myFirstCommit

---
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		greet();
+	}
+
+	public static void greet() {
+		System.out.println("welcome");
+	}
+}
